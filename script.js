6aa3a61c92fd4120d98bde86e77f0aee2e3759c4
spichay <seanpichay1015@gmail.com>
2024-09-30 20:24:12 -0400

    Created Event Listeners and Functions

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const searchQuery = document.getElementById("searchbar");
+const searchQueryVal = searchQuery.value;
+const searchButton = document.getElementById("search-btn");
+const randomSearchButton = document.getElementById("random-search-btn");
+
 function displaySearch(){
-    
+    alert("You searched: " + searchQueryVal);
 }
+
+function randomMessage() {
+    const messageArr = ["Feeling Hungry", "Feeling Tired", "Feeling Bored", "Feeling Sad", "Feeling Happy"];
+    const randomIndex = Math.floor(Math.random() * messageArr.length)
+    randomSearchButton.value = messageArr[randomIndex];
+}
+
+function defaultMessage() {
+    randomSearchButton.value = "Feeling Lucky";
+}
+
+searchButton.addEventListener("click", displaySearch());
+randomSearchButton.addEventListener("mouseover", randomMessage());
+randomSearchButton.addEventListener("mouseout", defaultMessage());
