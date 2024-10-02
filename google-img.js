c9aefa3e0d3136515ed5477608fed35e2185b77f
spichay <seanpichay1015@gmail.com>
2024-10-02 13:28:26 -0400

    Create Google Image Page

---
+++ google-img.js
@@ -0,0 +1,9 @@
+const imgSearchButton = document.getElementById("img-search-btn");
+const searchBar = document.getElementById("searchbar");
+
+function imgSearch() {
+    const searchVal = searchBar.value;
+    window.open(`https://www.google.com/search?q=${searchVal}&udm=2`);
+}
+
+imgSearchButton.addEventListener("click", imgSearch);
