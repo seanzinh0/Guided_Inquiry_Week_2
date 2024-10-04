9dd0b4c8892ad5181735fdbe75ccade505bd54cc
spichay <seanpichay1015@gmail.com>
2024-10-04 10:58:13 -0400

    FINALLY GOT THE ADVANCED SEARCH FUNCTION TO WORK

--- advanced-search.js
+++ advanced-search.js
@@ -4,17 +4,42 @@ const anyWords = document.getElementById("any-words");
 const noneWords = document.getElementById("none-words");
 const numStart = document.getElementById("num-start");
 const numEnd  = document.getElementById("num-end");
-const allWordsVal = allWords.value;
-const exactWordsVal = exactWords.value;
-const anyWordsVal = anyWords.value;
-const noneWordsVal = noneWords.value;
-const numStartVal = numStart.value;
-const numEndVal = numEnd.value;
 const advSearchBtn = document.getElementById("adv--search-btn");
 
 function advancedSearch(){
-    
+    const allWordsVal = allWords.value;
+    const exactWordsVal = exactWords.value;
+    const anyWordsVal = anyWords.value;
+    const noneWordsVal = noneWords.value;
+    const numStartVal = numStart.value;
+    const numEndVal = numEnd.value;
+    let finalExactWords;
+    let finalAnyWords;
+    let finalNoneWords;
+    let timeRange;
+    if(exactWordsVal){
+        finalExactWords = `"${allWordsVal}"`
+    }else if(exactWordsVal === ""){
+        finalExactWords = "";
+    }if(anyWordsVal){
+        finalAnyWords = anyWordsVal.replace(/ /g, " OR ");
+    }else if(anyWordsVal === ""){
+        finalAnyWords = "";
+    }if(noneWordsVal){
+        finalNoneWords = `-${noneWordsVal}`;
+    }else if(noneWordsVal === ""){
+        finalNoneWords = "";
+    }if(numStartVal && numEndVal){
+        timeRange = numStartVal + ".." + numEndVal;
+    }else if(numStartVal === "" && numEndVal === ""){
+        timeRange = "";
+    }
+
+    window.open(`https://google.com/search?q=${allWordsVal} ${finalExactWords} ${finalAnyWords} ${finalNoneWords} ${timeRange}`);
 }
+
+advSearchBtn.addEventListener("click", advancedSearch);
+
 
 
 
