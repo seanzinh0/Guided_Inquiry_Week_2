8765c980a747b946401f9b816c95223d4a113abd
spichay <seanpichay1015@gmail.com>
2024-10-04 09:30:34 -0400

    Initialize variables to create functionality for advanced search

---
+++ advanced-search.js
@@ -0,0 +1,20 @@
+const allWords = document.getElementById("all-words");
+const exactWords = document.getElementById("exact-words");
+const anyWords = document.getElementById("any-words");
+const noneWords = document.getElementById("none-words");
+const numStart = document.getElementById("num-start");
+const numEnd  = document.getElementById("num-end");
+const allWordsVal = allWords.value;
+const exactWordsVal = exactWords.value;
+const anyWordsVal = anyWords.value;
+const noneWordsVal = noneWords.value;
+const numStartVal = numStart.value;
+const numEndVal = numEnd.value;
+const advSearchBtn = document.getElementById("adv--search-btn");
+
+function advancedSearch(){
+    
+}
+
+
+
